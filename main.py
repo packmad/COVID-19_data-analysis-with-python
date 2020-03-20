25d79e83a65b9bd47345842f0b0359ea7d190889
Packmad <simone.aonzo@gmail.com>
2020-03-20 23:58:13 +0100

    zoom plot

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 from os.path import abspath, join
 from pandas import DataFrame
+from numpy import ndarray
+
 
 basedir = abspath(os.path.dirname(__file__))
 csv_files = join(basedir, 'csv_files')
@@ -13,6 +15,19 @@ out_figs = join(basedir, 'out_figs')
 def percentage(part: float, tot: float) -> float:
     assert part < tot
     return part * 100 / tot
+
+
+def plot_save(ax, x: ndarray, y: ndarray, labels: ndarray):
+    ax.set_xlabel('Confirmed/Population %')
+    ax.set_ylabel('Test/Population %')
+    ax.scatter(x, y, s=10, marker='x')
+    txt_offset = 0.0003
+    for i, txt in enumerate(labels):
+        ax.annotate(txt,
+                    xy=(x[i], y[i]),
+                    xytext=(x[i] + txt_offset, y[i] + txt_offset),
+                    color='#1f77b4',
+                    fontsize=6)
 
 
 if __name__ == "__main__":
@@ -44,11 +59,14 @@ if __name__ == "__main__":
     x = res['Confirmed/Population'].to_numpy()
     y = res['Test/Population'].to_numpy()
     labels = res['Entity'].to_numpy()
-    fig, ax = plt.subplots()
-    ax.set_xlabel('Confirmed/Population %')
-    ax.set_ylabel('Test/Population %')
-    ax.scatter(x, y, s=10, marker='x')
-    txt_offset = 0.0003
-    for i, txt in enumerate(labels):
-        ax.annotate(txt, xy=(x[i], y[i]), xytext=(x[i] + txt_offset, y[i] + txt_offset))
-    fig.savefig(join(out_figs, 'Test-Confirmed-Population.png'))
+
+    fig, ax = plt.subplots(2)
+    plot_save(ax[0], x, y, labels)
+
+    x = x[5:, ]
+    y = y[5:, ]
+    labels = labels[5:,]
+    plot_save(ax[1], x, y, labels)
+
+    fig.set_figheight(8)
+    fig.savefig(join(out_figs, f'Test-Confirmed-Population.png'))
